70520ca9b5d6039af30feff2f0d3a90d3769839b
Kotikov <11deadsluts@gmail.com>
2023-02-16 00:45:31 +0300

    simple UI is added

---
+++ user_interface.py
@@ -0,0 +1,8 @@
+import set_sample as ss
+
+def user_input ():
+    user_input = ss.get_data(input('Input your problem/task here: '))
+    return user_input
+
+# def user_output(user_input, result):
+#     print('{}={}'.format(user_input, result))
