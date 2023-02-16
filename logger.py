5b4e51017ccddcf5ceb2e296e6a5d04a935f2bca
Kotikov <11deadsluts@gmail.com>
2023-02-17 00:24:31 +0300

    logger is ready

--- logger.py
+++ logger.py
@@ -1,6 +1,15 @@
-def log_data_file_add (data):
-    with open ('log.json', 'a') as log_writing:
-        log_writing.write(data)
-def log_data_file_open ():
-    with open ('log.json', 'a') as log_reading:
-        print (*log_reading.readlines)
+import json
+import datetime
+
+
+def add_log(data):
+    with open('log.json', 'a') as log_writing:
+        dt = str(datetime.datetime.now())
+        log_writing.write('-'*6+ dt + '\n')
+        json.dump(data, log_writing, indent=4)
+        log_writing.write('-'*6+ '\n')
+
+
+def write_log():
+    with open('log.json', 'a') as log_reading:
+        print(*log_reading.readlines)
